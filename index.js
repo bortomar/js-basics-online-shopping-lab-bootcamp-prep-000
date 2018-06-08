83eb4ea29cf7641a02f46713ed0a723490bff96c
Mario Bortolotti <bortomar@email.cz>
2018-06-08 08:31:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push(item);
+ cart.push({ itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1});
 }
 
 function viewCart() {
